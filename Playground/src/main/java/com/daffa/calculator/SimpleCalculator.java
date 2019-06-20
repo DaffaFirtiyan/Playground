6ee9c5ac5f1802912ec27de0a984b6dc38e8637c
Daffa Firtiyan <mdaffa.anzalna@gmail.com>
2019-06-20 15:51:07 +1200

    Initial calculator commit

---
+++ Playground/src/main/java/com/daffa/calculator/SimpleCalculator.java
@@ -0,0 +1,23 @@
+package main.java.com.daffa.calculator;
+import java.util.Scanner;
+
+
+/**
+ * @author Daffa Firtiyan
+ *	A simple calculator program that scans keyboard input and add it with each other
+ */
+public class SimpleCalculator {
+
+	
+	public static void main(String args[]) {
+		Scanner keyboardInput = new Scanner(System.in);
+		System.out.print("Enter first number: ");
+		double num1 = keyboardInput.nextDouble();
+		
+		System.out.print("Enter second number: ");
+		double num2 = keyboardInput.nextDouble();
+		
+		System.out.println(num1+num2);
+	}
+
+}
