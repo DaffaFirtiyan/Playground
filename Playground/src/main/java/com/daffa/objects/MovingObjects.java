1a47f76fde5eda8796962a7ee43e81a0adbc0265
Daffa Firtiyan <mdaffa.anzalna@gmail.com>
2019-06-20 16:47:08 +1200

    Swapped ball for square

--- Playground/src/main/java/com/daffa/objects/MovingObjects.java
+++ Playground/src/main/java/com/daffa/objects/MovingObjects.java
@@ -3,7 +3,7 @@ package main.java.com.daffa.objects;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.*;
 
@@ -12,16 +12,16 @@ public class MovingObjects extends JPanel implements ActionListener{
 	Timer t = new Timer(5, this);
 	double x = 0;
 	double y = 0;
-	double vX = 1;
-	double vY = 1;
+	double vX = 2;
+	double vY = 2;
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
 		Graphics2D g2 = (Graphics2D) g;
-		Ellipse2D circle = new Ellipse2D.Double(x, y, 40, 40);
+		Rectangle2D square = new Rectangle2D.Double(x, y, 40, 40);
 		
-		g2.fill(circle);
+		g2.fill(square);
 		t.start();
 	}
 	
